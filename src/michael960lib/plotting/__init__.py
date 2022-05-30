df19e85bc585612106b39f3424301a9ec4f1c13e
michael <mike.wang96029@gapp.nthu.edu.tw>
2022-05-30 17:27:59 +0800

    complex rgb function for plotting

--- src/michael960lib/plotting/__init__.py
+++ src/michael960lib/plotting/__init__.py
@@ -1,1 +1,1 @@
-from .plot_helper import *
+from .plot_helper import first_peak, val_to_ind, second_der, test_pol, complex_array_to_rgb
--- src/michael960lib/plotting/plot_helper.py
+++ src/michael960lib/plotting/plot_helper.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import optimize
+from matplotlib import pyplot as plt
+import matplotlib
 
 def first_peak(S):
     m = -1
@@ -40,6 +42,24 @@ def first_der(x, v, i, rng):
 
 def test_pol(x, a, b, c, d, e, f, g):
     return a + b*x + c*x**2 + d*x**3 + e*x**4 + f*x**5 + g*x**6
+
+
+
+def complex_array_to_rgb(X, theme='dark', rmax=None):
+    '''Takes an array of complex number and converts it to an array of [r, g, b],
+    where phase gives hue and saturaton/value are given by the absolute value.
+    Especially for use with imshow for complex plots.'''
+    absmax = rmax or np.abs(X).max()
+    Y = np.zeros(X.shape + (3,), dtype='float')
+    Y[..., 0] = (np.angle(X)) / (2 * np.pi) % 1
+    if theme == 'light':
+        Y[..., 1] = np.clip(np.abs(X) / absmax, 0, 1)
+        Y[..., 2] = 1
+    elif theme == 'dark':
+        Y[..., 1] = 1
+        Y[..., 2] = np.clip(np.abs(X) / absmax, 0, 1)
+    Y = matplotlib.colors.hsv_to_rgb(Y)
+    return Y
 
 
 
